4c2b22e1c21376ff983959f38bc5b711f4761a32
Adriel Klein <adrielklein@Adriels-MBP.home>
2016-04-09 16:29:36 -0400

    refactor test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,13 @@
 import pytest
 from app.app import create_app
+from app.corpus import Corpus
 
 
 @pytest.fixture
 def app():
     return create_app()
+
+
+@pytest.fixture
+def corpus():
+    return Corpus()
