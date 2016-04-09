b6e38abbc2853300da33b6c5ddba38ae583c2407
Adriel Klein <adrielklein@Adriels-MacBook-Pro.local>
2016-04-09 10:34:41 -0400

    make acknowledge route

---
+++ tests/test_acknowledge_route.py
@@ -0,0 +1,4 @@
+def test_when_acknowledge_route_is_hit_then_it_return_okay(app):
+    with app.test_client() as test_client:
+        response = test_client.get('/')
+        assert 'OK' == response.data.decode()
