ec36feeca5ae1f213d7a8c2413b37171d1e8b7cb
Adriel Klein <adrielklein@Adriels-MacBook-Pro.local>
2016-04-09 11:19:54 -0400

    refactor: make a RouteCreator object that will take in the DataStore

--- app/app.py
+++ app/app.py
@@ -1,14 +1,10 @@
-from app.routes import acknowledge_route
+from app.routes import RouteCreator
 from flask import Flask
 
 
 def create_app():
     app = Flask(__name__)
-
-    _set_up_routes(app)
+    route_creator = RouteCreator(app)
+    route_creator.set_up_routes()
 
     return app
-
-
-def _set_up_routes(app):
-    app.add_url_rule('/', 'acknowledge_route', acknowledge_route)
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,9 @@
-def acknowledge_route():
-    return 'OK'
+class RouteCreator(object):
+    def __init__(self, app):
+        self.app = app
+
+    def set_up_routes(self):
+        self.app.add_url_rule('/', 'acknowledge_route', self._acknowledge_route)
+
+    def _acknowledge_route(self):
+        return 'OK'
