dcb880d2879e7462368845ed4fc3299ece90027b
Adriel Klein <adrielklein@Adriels-MBP.home>
2016-04-11 22:38:36 -0400

    split test up

--- tests/corpus/test_corpus_operations.py
+++ tests/corpus/test_corpus_operations.py
@@ -41,39 +41,3 @@ def test_when_limit_is_specified_for_maximum_number_of_anagrams_then_do_not_exce
 def test_when_requesting_anagrams_of_words_that_are_not_in_corpus_yet_then_return_nothing(corpus):
     corpus.add_words(['read'])
     assert [] == corpus.get_anagrams('dare')
-
-
-def test_when_stats_method_is_called_then_return_stats(corpus):
-    corpus.add_words(['it', 'dog', 'person'])
-    stats = corpus.get_stats()
-    assert 5 == len(stats)
-    assert 3 == stats['count']
-    assert 2 == stats['min_length']
-    assert 6 == stats['max_length']
-    assert 3 == stats['med_length']
-    assert 11 / 3 == stats['ave_length']
-
-
-def test_when_stats_method_is_called_with_no_words_then_return_stats(corpus):
-    stats = corpus.get_stats()
-    assert 5 == len(stats)
-    assert 0 == stats['count']
-    assert 0 == stats['min_length']
-    assert 0 == stats['max_length']
-    assert 0 == stats['med_length']
-    assert 0 == stats['ave_length']
-
-
-def test_when_most_anagrams_method_is_called_then_words_with_most_anagrams(corpus):
-    corpus.add_words(['read', 'dare'])
-    corpus.add_words(['brag', 'grab'])
-    corpus.add_words(['it'])
-    alphagram_to_words = corpus.get_most_anagrams()
-    assert 2 == len(alphagram_to_words)
-    assert ['dare', 'read'] == sorted(alphagram_to_words['ader'])
-    assert ['brag', 'grab'] == sorted(alphagram_to_words['abgr'])
-
-
-def test_when_there_are_no_words_then_get_empty_hash_table(corpus):
-    alphagram_to_words = corpus.get_most_anagrams()
-    assert 0 == len(alphagram_to_words)
