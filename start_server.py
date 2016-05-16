e61916fa9327cc61cbadde4743194cacd1e6b43e
Adriel Klein <adrielmklein@gmail.com>
2016-05-16 11:07:51 -0400

    add index.html and modify start server

--- start_server.py
+++ start_server.py
@@ -2,8 +2,8 @@ import os
 
 from app.main import create_app
 
+PORT = 5000
 if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', PORT))
     app = create_app()
     app.run(host='0.0.0.0', port=port)
